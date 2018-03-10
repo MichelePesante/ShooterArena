c9d9977a109bd07d3616ba1a47f84778cd45a29b
Michele Pesante <pesante.michele.96@gmail.com>
2018-03-10 13:42:28 +0100

    Implementata scena e player con script di input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	private float movementSpeed;
+	private float jumpSpeed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		movementSpeed = 0.2f;
+		jumpSpeed = 20f;
+		rb = gameObject.GetComponent<Rigidbody> ();
+		if (!gameObject.GetComponent<Rigidbody> ())
+			rb = gameObject.AddComponent<Rigidbody> ();
+		rb.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += Vector3.left * movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += Vector3.right * movementSpeed;
+		}
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rb.velocity = Vector3.up * jumpSpeed;
+		}
+	}
+}
