c0f35b2d5d036570a236232254eb50568b08aa47
Michele Pesante <pesante.michele.96@gmail.com>
2018-03-10 14:14:42 +0100

    Aggiunto script bullet e grafica granata

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private float bulletSpeed;
+
+	private BulletState CurrentBulletState;
+
+	// Use this for initialization
+	void Start () {
+		CurrentBulletState = BulletState.InPool;
+		bulletSpeed = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public enum BulletState {
+		InPool = 0,
+		InScene = 1
+	}
+}
