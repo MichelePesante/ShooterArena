73228b25adbe823b0fe8cce629bbb899a4fd520f
Riccardo Mazzocchi <rikuz91@hotmail.com>
2018-03-10 20:17:22 +0100

    Aggiunto testo che mostra quanti player sono attivi

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
 
     public static int numberOfPlayers;
 
     bool p1, p2, p3, p4;
+
+    public Text player1text, player2text, player3text, player4text;
 
     // Use this for initialization
     void Start () {
@@ -20,26 +23,31 @@ public class PlayerManager : MonoBehaviour {
         {
             numberOfPlayers++;
             p1 = true;
+            player1text.enabled = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Joystick2Button7) && !p2)
         {
             numberOfPlayers++;
             p2 = true;
+            player2text.enabled = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Joystick3Button7) && !p3)
         {
             numberOfPlayers++;
             p3 = true;
+            player3text.enabled = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Joystick4Button7) && !p4)
         {
             numberOfPlayers++;
             p4 = true;
+            player4text.enabled = true;
         }
 
+        
         Debug.Log(numberOfPlayers);
     }
 }
