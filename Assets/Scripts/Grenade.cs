1381b88aa638b59b6bc6c1fc549e6b150b146dc1
GiacomoPignoni <giacomo.pignoni@gmail.com>
2018-03-10 14:34:52 +0100

    Aggiunta granata

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+	public GrenadeState CurrentGrenadeState;
+
+	void Start () {
+		CurrentGrenadeState = GrenadeState.InPool;
+	}
+		
+	void Update () {
+
+	}
+
+	public enum GrenadeState {
+		InPool = 0,
+		InScene = 1
+	}
+}
